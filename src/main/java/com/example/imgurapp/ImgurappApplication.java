5f81af93f24a3e18462845f01e39f512b2fb2a42
Ramya <ramyat7842@gmail.com>
2024-12-09 20:50:18 +0530

    Created App through Spring initializr

---
+++ src/main/java/com/example/imgurapp/ImgurappApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgurapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgurappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgurappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imgurapp/ImgurappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imgurapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgurappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
